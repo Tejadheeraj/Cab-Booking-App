cc9d9c06074c42f9d0434aca43641a868f720125
Tejadheeraj <venkataallimalli@gmail.com>
2025-04-02 13:57:56 +0530

    upload

---
+++ frontend/src/pages/Start.jsx
@@ -0,0 +1,39 @@
+// import React from 'react'
+// import { Link } from 'react-router-dom'
+
+// const Start = () => {
+//   return (
+//     <div>
+//         <div className='bg-cover bg-center bg-[url(cab.png)] h-screen pt-8 flex justify-between flex-col w-full '>
+//             <img className='w-16 ml-8' src="https://download.logo.wine/logo/Uber/Uber-Logo.wine.png" alt="" />
+//             <div className='bg-white pb-7 py-4 px-4' >
+//                 <h2 className='text-[30px] font-bold'>Get Started</h2>
+//                 <Link to='/login' className='flex items-center justify-center w-full bg-black text-white py-3 rounded-lg mt-5'>Continue</Link>
+//             </div>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Start
+
+
+
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Start = () => {
+  return (
+    <div>
+      <div className='bg-cover bg-center bg-[url(cab.png)] h-screen pt-8 flex justify-between flex-col w-full'>
+        <img className='w-16 ml-8' src="https://download.logo.wine/logo/Uber/Uber-Logo.wine.png" alt="" />
+        <div className='bg-white pb-8 py-4 px-4'>
+          <h2 className='text-[30px] font-semibold'>Get Started with Uber</h2>
+          <Link to='/login' className='flex items-center justify-center w-full bg-black text-white py-3 rounded-lg mt-5'>Continue</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Start
