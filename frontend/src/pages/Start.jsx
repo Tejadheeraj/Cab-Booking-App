ab5fd7d5f990d5240d805ea0dc7da16309a19140
Tejadheeraj <venkataallimalli@gmail.com>
2025-09-04 22:49:31 +0530

    Updated img

--- frontend/src/pages/Start.jsx
+++ frontend/src/pages/Start.jsx
@@ -25,7 +25,7 @@ import { Link } from 'react-router-dom'
 const Start = () => {
   return (
     <div>
-      <div className='bg-cover bg-center bg-[url(cab.png)] h-screen pt-8 flex justify-between flex-col w-full'>
+      <div className='bg-cover bg-center bg-[url("/cab.png")] h-screen pt-8 flex justify-between flex-col w-full'>
         <img className='w-16 ml-8' src="https://download.logo.wine/logo/Uber/Uber-Logo.wine.png" alt="" />
         <div className='bg-white pb-8 py-4 px-4'>
           <h2 className='text-[30px] font-semibold'>Get Started with Uber</h2>
