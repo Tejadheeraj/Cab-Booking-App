6d6bcfbb56ee8024c7508d174f2f010b24953697
Tejadheeraj <venkataallimalli@gmail.com>
2025-09-04 20:19:52 +0530

    Updated blacklist token model

--- Backend/app.js
+++ Backend/app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.json({ status: 'Server running' });
 });
 
 app.use('/users', userRoutes);
